0534600501e75f64512e2cec2ee902edb0375306
Marcos Rigel <marcosrigel99@gmail.com>
2022-05-24 11:22:54 -0300

    Problemas resolvidos em Java

---
+++ Java/URI-1001/src/Program.java
@@ -0,0 +1,44 @@
+/* Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. 
+ * Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado 
+ * e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+ * 
+ * Entrada
+ * A entrada contém 2 valores inteiros.
+ * 
+ * Saída
+ * Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha. Cuide para que tenha um espaço
+ * antes e depois do sinal de igualdade, conforme o exemplo abaixo.
+ * 
+ * Exemplo
+ * ========================================================================
+ * Entrada						Saída
+ * ========================================================================
+ * 10							X = 19
+ * 9
+ * ========================================================================
+ * -10							X = -6
+ * 4
+ * ========================================================================
+ * 15							X = 8
+ * -7
+ */
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int soma = a + b;
+		
+		System.out.println("X = " + soma);
+		
+		sc.close();
+	}
+
+}
