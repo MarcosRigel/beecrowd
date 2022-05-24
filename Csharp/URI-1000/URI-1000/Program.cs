1d815cf6b8460d35fa0bf41c2b6bf4cfb299485b
Marcos Rigel <marcosrigel99@gmail.com>
2022-05-24 11:00:53 -0300

    problemas resolvidos em csharp

---
+++ Csharp/URI-1000/URI-1000/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Bem-vindo ao beecrowd!
+ * 
+ * O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+ * 
+ * Entrada
+ * Este problema não possui nenhuma entrada.
+ * 
+ * Saída
+ * Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+ * 
+ * 
+ */
+
+using System;
+
+namespace URI_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
