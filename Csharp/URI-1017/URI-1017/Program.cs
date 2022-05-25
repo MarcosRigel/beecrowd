3ffa158382e8a8e8512987ff7c29079ab95cac19
Marcos Rigel <marcosrigel99@gmail.com>
2022-05-25 11:39:22 -0300

    problemas resolvidos em csharp

---
+++ Csharp/URI-1017/URI-1017/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L. 
+ * Para isso, ele gostaria que você o auxiliasse através de um simples programa. Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas)
+ * e a velocidade média durante a mesma (em km/h). Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. 
+ * Mostre o valor com 3 casas decimais após o ponto.
+ * 
+ * Entrada
+ * O arquivo de entrada contém dois inteiros. O primeiro é o tempo gasto na viagem (em horas) e o segundo é a velocidade média durante a mesma (em km/h).
+ * 
+ * Saída
+ * Imprima a quantidade de litros necessária para realizar a viagem, com três dígitos após o ponto decimal
+ * 
+ * Exemplo
+ * ====================================================================
+ * Entrada                                          Saída
+ * ====================================================================
+ * 10                                               70.833
+ * 85
+ * ====================================================================
+ * 2                                                15.333
+ * 92
+ * ====================================================================
+ * 22                                               122.833
+ * 67
+ */
+
+using System;
+using System.Globalization;
+
+namespace Primerio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempoGasto, velocidadeMedia, total;
+            double quantidadeLitros;
+
+            tempoGasto = int.Parse(Console.ReadLine());
+            velocidadeMedia = int.Parse(Console.ReadLine());
+
+            total = tempoGasto * velocidadeMedia;
+
+            quantidadeLitros = (double)total / 12.0;
+
+            Console.WriteLine(quantidadeLitros.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
