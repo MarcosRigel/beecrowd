4e25043bf42eb86eda1b8dddff2c382165c6f9d9
R1GEL <63622155+MarcosRigel@users.noreply.github.com>
2023-03-05 23:42:43 -0300

    fibonacci

--- Java/URI-1051/src/Main.java
+++ Java/URI-1051/src/Main.java
@@ -1,36 +1,34 @@
-import java.util.Locale;
+package application;
+
 import java.util.Scanner;
 
-public class Main {
+public class Program {
 
-    public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        
-        double salario;
-        double imposto = 0.0;
-        double diferenca;
-        
-        salario = sc.nextDouble();
-        
-        if(salario > 4500.0) {
-            imposto = 1000.0 * 0.08 + 1500.0 * 0.18;
-            diferenca = salario - 4500.0;
-            imposto += diferenca * 0.28;
-        } else if(salario > 3000.0) {
-            imposto = 1000 * 0.08;
-            diferenca = salario - 3000.0;
-            imposto += diferenca * 0.18;
-        } else if(salario > 2000.0) {
-            diferenca = salario - 2000.0;
-            imposto = diferenca * 0.08;
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+        int Fn = 0;
+        int fn = 1;
+
+        for (int i = 0; i < N; i++)
+        {
+            if (i == N - 1)
+            {
+            	System.out.print(Fn + " ");
+            }
+            else
+            {
+            	System.out.print(Fn + " ");
+            }
+            int aux = fn;
+            fn = Fn;
+            Fn = aux + fn;
         }
-        if (imposto > 0.0) {
-            System.out.printf("R$ %.2f%n", imposto);
-        } else {
-            System.out.println("Isento");
-        }    
-        
-        sc.close();
-    }
+		
+		sc.close();
+		
+	}
+
 }
