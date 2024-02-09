d2da5913b4ca5f706b08308bed915a66a86470e5
raffaellng <39544560+raffaellng@users.noreply.github.com>
2024-02-09 01:36:08 -0300

    URI - 1018
    Refatorando - Encontrado uma melhor forma da solução do problema

--- Csharp/URI-1018/URI-1018/Program.cs
+++ Csharp/URI-1018/URI-1018/Program.cs
@@ -49,23 +49,16 @@ namespace URI_1018
     {
         static void Main(string[] args)
         {
-            int N = int.Parse(Console.ReadLine());
+            int[] cedulas = { 100, 50, 20, 10, 5, 2, 1 };
+            int valor = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(valor);
 
-            Console.WriteLine(N);
-
-            Console.WriteLine(N / 100 + " nota(s) de R$ 100,00");
-            N = N % 100;
-            Console.WriteLine(N / 50 + " nota(s) de R$ 50,00");
-            N = N % 50;
-            Console.WriteLine(N / 20 + " nota(s) de R$ 20,00");
-            N = N % 20;
-            Console.WriteLine(N / 10 + " nota(s) de R$ 10,00");
-            N = N % 10;
-            Console.WriteLine(N / 5 + " nota(s) de R$ 5,00");
-            N = N % 5;
-            Console.WriteLine(N / 2 + " nota(s) de R$ 2,00");
-            N = N % 2;
-            Console.WriteLine(N / 1 + " nota(s) de R$ 1,00");
+            foreach (int nota in cedulas)
+            {
+                int quantidadeNotas = valor / nota;
+                Console.WriteLine($"{quantidadeNotas} nota(s) de R$ {nota},00");
+                valor %= nota;
+            }
         }
     }
 }
